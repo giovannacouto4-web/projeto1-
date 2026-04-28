062d507ed15635d10456bb149ddd99a7ece7070c
giovannacouto4-web <giovannacouto4@gmail.com>
2026-04-28 15:47:42 -0300

    Create rockinrio.py

---
+++ rockinrio.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.title('Será que o Justin Bieber virá para o Rock in Rio?')
+st.write("O último show de Justin Bieber foi no Rock in Rio de 2022. De lá para cá, o cantor não voltou a subir nos palcos. Mas o perfil oficial do festival atiçou os fãs nesta semana, com um comentário em uma foto do cantor no Instagram. “Tive uma ideia aqui… o que acham?”, escreveu o @rockinrio.")
+st.image('https://poptivo.com.br/wp-content/uploads/2024/01/photo_2024-01-19_17-10-51-300x280.jpg')
