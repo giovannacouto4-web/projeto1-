7d765ba454c1cc0a7e45118419c6b60abb0f74f2
giovannacouto4-web <giovannacouto4@gmail.com>
2026-04-28 15:15:45 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -2,4 +2,4 @@ import streamlit as st
 
 st.title('App da gica + cachorros')
 st.write("Cachorrinhos engraçadinhos e fofinhos")
-st.image("https://br.pinterest.com/pin/849702654685758044/")
+st.image("https://www.facebook.com/photo/?fbid=451573140409810&set=pb.100066713845917.-2207520000")
