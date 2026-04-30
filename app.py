adedd11f04fe39017df2fe6af9108e69f7ba8522
giovannacouto4-web <giovannacouto4@gmail.com>
2026-04-29 21:12:55 -0300

    Update app.py

--- app.py
+++ app.py
@@ -21,11 +21,6 @@ if st.button("Decidir"):
         st.session_state.historico.append(escolha)
     else:
         st.warning("Digite pelo menos uma opção!")
-   
-if feedback == "Sim":
-    st.success("Que bom! 😄")
-elif feedback == "Não":
-    st.info("Que tal tentar novamente? 😉")
 
 
 if st.session_state.historico:
