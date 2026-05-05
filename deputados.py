1599f6abecf72179459686842d02f5dbe2ddc0ec
giovannacouto4-web <giovannacouto4@gmail.com>
2026-05-05 15:30:02 -0300

    Update deputados.py

--- deputados.py
+++ deputados.py
@@ -1,8 +1,13 @@
 import streamlit as st
-import pandas as pd 
+import pandas as pd
+
 df = pd.read_csv('deputados_2022.csv')
 
 partidos = df["partido"].unique
+partido_selecionado = st.selectbox("Escolha um partido", partidos)
+
+deputados_filtrados = df[df["partido"] == partido_selecionado]
+
 st.title("Consulta aos deputados de 2022")
 
 st.dataframe(deputados_filtrados)
