088e929284978e18d3fdce01c2f68f009aa8e281
giovannacouto4-web <giovannacouto4@gmail.com>
2026-05-05 14:57:38 -0300

    Update deputados.py

--- deputados.py
+++ deputados.py
@@ -1,16 +1,11 @@
 import streamlit as st
 import pandas as pd 
 df = pd.read_csv('deputados_2022.csv')
-st.dataframe(df)
 
-# Pegar lista de partidos únicos
 partidos = df["partido"].unique()
 
-# Criar a caixa de seleção
 partido_selecionado = st.selectbox("Escolha um partido:", partidos)
 
-# Filtrar os deputados do partido escolhido
 deputados_filtrados = df[df["partido"] == partido_selecionado]
 
-# Mostrar resultado
 st.dataframe(deputados_filtrados)
