36d053b3eaada82da0218565ced539d6a56b30c9
giovannacouto4-web <giovannacouto4@gmail.com>
2026-05-05 15:23:46 -0300

    Update deputados.py

--- deputados.py
+++ deputados.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd 
 df = pd.read_csv('deputados_2022.csv')
+
 st.title("Consulta aos deputados de 2022")
 
 partidos = df["partido"].unique()
@@ -14,10 +15,12 @@ st.dataframe(deputados_filtrados)
 partidos = st.text_input('Digite o partido que você queira ver os deputados:')
 uf = st.text_input('Digite a UF')
 
-if sigla:
-  df_filtrado = df[df['partido'] == sigla.upper()]
+if partidos:
+  df_filtrado = df[df['partido'] == partidos.upper()]
 else: 
   df_filtrado = df
   
 if uf: 
   df_filtrado = df_filtrado[df_filtrado['uf'] == uf.upper()]
+
+st.dataframe(df_filtrado)
