314ef03d5ccb4655f677abd853267925b5898431
giovannacouto4-web <giovannacouto4@gmail.com>
2026-05-05 15:25:46 -0300

    Update deputados.py

--- deputados.py
+++ deputados.py
@@ -3,13 +3,6 @@ import pandas as pd
 df = pd.read_csv('deputados_2022.csv')
 
 st.title("Consulta aos deputados de 2022")
-
-partidos = df["partido"].unique()
-
-partido_selecionado = st.selectbox("Escolha um partido:", partidos)
-
-deputados_filtrados = df[df["partido"] == partido_selecionado]
-
 st.dataframe(deputados_filtrados)
 
 partidos = st.text_input('Digite o partido que você queira ver os deputados:')
