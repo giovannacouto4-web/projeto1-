161b70ecedb999c04d5349d6ed3110c4332f7aa6
giovannacouto4-web <giovannacouto4@gmail.com>
2026-05-05 14:55:43 -0300

    Update deputados.py

--- deputados.py
+++ deputados.py
@@ -3,11 +3,14 @@ import pandas as pd
 df = pd.read_csv('deputados_2022.csv')
 st.dataframe(df)
 
-print(df.columns)
-partidos = df["partido"]
-st.dataframe(partidos)
-partidos = df[["partido"]]
-st.dataframe(partidos)
-partidos_unicos = df["partido"].unique()
-st.write(partidos_unicos)
+# Pegar lista de partidos únicos
+partidos = df["partido"].unique()
 
+# Criar a caixa de seleção
+partido_selecionado = st.selectbox("Escolha um partido:", partidos)
+
+# Filtrar os deputados do partido escolhido
+deputados_filtrados = df[df["partido"] == partido_selecionado]
+
+# Mostrar resultado
+st.dataframe(deputados_filtrados)
