c89d35151f3a40c769c31c489c056a42d12f00a2
giovannacouto4-web <giovannacouto4@gmail.com>
2026-05-05 15:30:44 -0300

    Update deputados.py

--- deputados.py
+++ deputados.py
@@ -10,17 +10,3 @@ deputados_filtrados = df[df["partido"] == partido_selecionado]
 
 st.title("Consulta aos deputados de 2022")
 
-st.dataframe(deputados_filtrados)
-
-partidos = st.text_input('Digite o partido que você queira ver os deputados:')
-uf = st.text_input('Digite a UF')
-
-if partidos:
-  df_filtrado = df[df['partido'] == partidos.upper()]
-else: 
-  df_filtrado = df
-  
-if uf: 
-  df_filtrado = df_filtrado[df_filtrado['uf'] == uf.upper()]
-
-st.dataframe(df_filtrado)
