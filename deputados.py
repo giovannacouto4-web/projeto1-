197239a47c0b61a434ca44d1b7b59e4e96aabe61
giovannacouto4-web <giovannacouto4@gmail.com>
2026-05-05 14:54:31 -0300

    Update deputados.py

--- deputados.py
+++ deputados.py
@@ -2,3 +2,12 @@ import streamlit as st
 import pandas as pd 
 df = pd.read_csv('deputados_2022.csv')
 st.dataframe(df)
+
+print(df.columns)
+partidos = df["partido"]
+st.dataframe(partidos)
+partidos = df[["partido"]]
+st.dataframe(partidos)
+partidos_unicos = df["partido"].unique()
+st.write(partidos_unicos)
+
