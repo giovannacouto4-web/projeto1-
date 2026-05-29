342350d4822366f498ec41c54774af1db45e7e70
giovannacouto4-web <giovannacouto4@gmail.com>
2026-05-28 21:19:18 -0300

    Update grafico.py

--- grafico.py
+++ grafico.py
@@ -7,7 +7,7 @@ df.columns = df.columns.str.strip()
  
 st.write("Colunas encontradas:", df.columns.tolist())
  
-st.title("👗 Marcas de Moda")
+st.title("Gráfico de Marcas de Moda")
  
 colunas_disponiveis = [c for c in df.columns if c != "marca"]
  
