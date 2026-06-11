89b2d99cae0b91e212aa618b6de0d16fbba54c2e
giovannacouto4-web <giovannacouto4@gmail.com>
2026-06-11 18:07:17 -0300

    Update installgeradordelegendas.py

--- installgeradordelegendas.py
+++ installgeradordelegendas.py
@@ -3,5 +3,5 @@ import os
 import google.generativeai as genai
 os.environ["GEMINI_API_KEY"] = 'Ab8RN6IbNfgDcgDc1jim4bbKV2eVKHZg-jiHwvgJtunUxtE2ZQ'
 
-genai.configure(api_key=os.environ["GEMINI_API_KEY"]
+genai.configure(api_key=os.environ["GEMINI_API_KEY"])
 model = genai.GenerativeModel("gemini-1.5-flash")
